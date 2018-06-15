59c0da9608351ac54ca8bf15ea7c5b4ab5a23df6
ChaKyungJun <dennnischa50@gmail.com>
2018-06-15 17:52:02 +0900

    1-b: Player movement and door animation

--- Door.cs
+++ Door.cs
@@ -3,27 +3,54 @@ using System.Collections;
 
 public class Door : MonoBehaviour
 {
-    private Transform step1;
-    private Transform step2;
+    private Transform doorTransform;
+    private Transform fStep;
+    private Transform bStep;
     private bool locked;
+    private Transform door;
     private Secret secret;
+    private Animator doorAnim;
+    int openhash = Animator.StringToHash("Open");
 
-    public bool Locked{
-        get { return this.locked;}
+
+    public bool initialLocked = false;
+
+    private void Start()
+    {
+        doorAnim = GetComponentInChildren<Animator>();   
+        doorTransform = transform.transform;
+        door = transform.Find("Door");
+        fStep = transform.Find("FStep").transform;
+        bStep = transform.Find("BStep").transform;
+        locked = initialLocked;
+    }
+
+    public bool Locked
+    {
+        get { return this.locked; }
     }
 
     public void Open()
     {
-        
+        doorAnim.SetTrigger(openhash);
     }
-
-    public void Close()
-    {
-        
-    }
-
     public void Touched()
     {
-        
+
+    }
+
+    public Transform DoorTransform
+    {
+        get { return doorTransform;}
+    }
+
+    public Transform FStep 
+    {
+        get { return fStep; }
+    }
+
+    public Transform BStep
+    {
+        get { return bStep; }
     }
 }
--- PlayerController.cs
+++ PlayerController.cs
@@ -1,15 +1,89 @@
 using UnityEngine;
 using System.Collections;
 
+delegate IEnumerator RotateAndMove(Vector3 v);
 public class PlayerController: MonoBehaviour
 {
-    //public static PlayerController instance = null;
     private GameObject player;
     private bool hasKey;
+    private bool moving;
+    private bool rotating;
+    private Vector3 direction;
+    private Quaternion lookRotation;
+    private Vector3 destination;
+    private RotateAndMove rotateAndMove;
 
-    public void MoveTo(Door door) 
+    private float movingSpeed = 1.5f;
+    private float rotationSpeed = 40f;
+    public Door tempDoor;
+
+
+    public PlayerController()
     {
-        
+        hasKey = false;
+        moving = false;
+        rotating = false;
+    }
+
+	void Start()
+	{
+        player = gameObject;
+        destination = gameObject.transform.position;
+        StartCoroutine(CoMoveToDoor(tempDoor));
+	}
+
+    void FixedUpdate()
+	{
+        if(moving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * movingSpeed);
+        }
+
+        if(rotating)
+        {
+            direction = (destination - transform.position).normalized;
+            lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+        }
+		
+	}
+
+    IEnumerator CoMoveTo(Vector3 point)
+    {
+        moving = true;
+        destination = point;
+        while(Vector3.Distance(transform.position,destination) > 0.1f)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        Debug.Log(transform.position);
+        moving = false;
+    }
+    IEnumerator CoRotateTo(Vector3 point)
+    {
+        rotating =true;
+        destination = point;
+        yield return new WaitForFixedUpdate();
+        while(Quaternion.Angle(transform.rotation, lookRotation) != 0)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        rotating = false;
+    }
+    public IEnumerator CoMoveToDoor(Door door) 
+    {
+        Vector3 height = new Vector3(0,transform.position.y,0);
+        Vector3 intermediatePoint = door.DoorTransform.position + height;
+        Vector3 goalPoint =((Vector3.Distance(transform.position, door.FStep.position) > Vector3.Distance(transform.position, door.BStep.position)) ? door.FStep.position : door.BStep.position);
+
+        door.Open();
+        yield return new WaitForSeconds(1f);
+
+        StartCoroutine(CoRotateTo(intermediatePoint));
+        yield return StartCoroutine(CoMoveTo(intermediatePoint));
+
+        StartCoroutine(CoRotateTo(goalPoint));
+        yield return StartCoroutine(CoMoveTo(goalPoint));
     }
 
     public bool HasKey
