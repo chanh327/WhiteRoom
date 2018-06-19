67a27a8633a7cce3c1a997cd2e0d180059045ef2
ChaKyungJun <dennnischa50@gmail.com>
2018-06-19 17:46:50 +0900

    Button Animation update  /Unity Team update

---
+++ Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+	private bool ison;
+	private Animator buttonAnim;
+
+	void Awake()
+	{
+		buttonAnim = GetComponent<Animator>();
+	}
+	void Start()
+	{
+		Ison = false;
+	}
+
+	public bool Ison
+	{
+		get {return ison;}
+		set 
+		{
+			Debug.Log(value);
+			buttonAnim.SetBool("ison", value);
+			ison = value;
+		}
+	}
+}
