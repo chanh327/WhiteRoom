b6151b869a910c14896fcca84eb21e1bd3b7160c
ChaKyungJun <dennnischa50@gmail.com>
2018-08-13 04:58:26 +0900

    TutorialScreen

---
+++ TutorialScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScreen : MonoBehaviour {
+	public Sprite[] sprites;
+	public float intervalTime;
+	private SpriteRenderer spriteRenderer;
+
+	void Awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Start()
+	{
+		StartCoroutine(Play());
+	}
+	private IEnumerator Play()
+	{
+		while(true)
+		{
+			for(int i = 0; i < sprites.Length; i++)
+			{
+				spriteRenderer.sprite = sprites[i];
+				yield return new WaitForSeconds(intervalTime);
+			}
+		}
+		yield return new WaitForSeconds(intervalTime * 3);
+	}
+}
